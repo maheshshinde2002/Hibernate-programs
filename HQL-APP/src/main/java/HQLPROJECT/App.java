c5b70e892d880da4f115881e72e20f1afdb02bcd
maheshshinde2002 <maheshshinde6023@gmail.com>
2025-11-08 10:03:28 +0530

    first commit

--- HQL-APP/src/main/java/HQLPROJECT/App.java
+++ HQL-APP/src/main/java/HQLPROJECT/App.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
@@ -23,14 +23,51 @@ public class App {
         s1.setName1("Akash");
         s1.setAddress1("Mumbai");
 
+        StudentAccess s2=new StudentAccess();
+        s2.setName1("Akash2");
+        s2.setAddress1("Mumbai2");
+
+        StudentAccess s3=new StudentAccess();
+        s3.setName1("Akash3");
+        s3.setAddress1("Mumbai3");
+
+        StudentAccess s4=new StudentAccess();
+        s4.setName1("Akash4");
+        s4.setAddress1("Mumbai4");
+
+        StudentAccess s5=new StudentAccess();
+        s5.setName1("Akash5");
+        s5.setAddress1("Mumbai5");
+
+        StudentAccess s6=new StudentAccess();
+        s6.setName1("Akash6");
+        s6.setAddress1("Mumbai6");
+
+        StudentAccess s7=new StudentAccess();
+        s7.setName1("Akash7");
+        s7.setAddress1("Mumbai7");
+
+        StudentAccess s8=new StudentAccess();
+        s8.setName1("Akash8");
+        s8.setAddress1("Mumbai8");
+
 
 
         Session session=factory.openSession();
         Transaction tx = session.beginTransaction();
-//        session.persist(s);
+//       session.persist(s);
 //        session.persist(s1);
-//
-//        tx.commit();
+//        session.persist(s2);
+//        session.persist(s3);
+//        session.persist(s4);
+//        session.persist(s5);
+//        session.persist(s6);
+//        session.persist(s7);
+//        session.persist(s8);
+
+
+
+    // tx.commit();
 
 //
 
@@ -44,11 +81,39 @@ public class App {
 //        int i =q.executeUpdate();
 //        System.out.println("Delete successfully");
 //
-        Query q= session.createQuery("update StudentAccess set address1='Rashin' where id1=1 ");
-        int i=q.executeUpdate();
-        System.out.println(i + "Updated successfully");
+//        Query q= session.createQuery("update StudentAccess set address1='Rashin' where id1=1 ");
+//        int i=q.executeUpdate();
+//        System.out.println(i + "Updated successfully");
+//
+//
+//        Query query=session.createQuery("from StudentAccess");
+//
+//        //impleminting pagination using hibernate
+//
+//        query.setFirstResult(4);
+//        query.setMaxResults(8);
+//
+//        List<StudentAccess> list =query.list();
+//        for(StudentAccess st:list){
+//            System.out.println(st.getId1()+" : "+st.getName1()+" :  "+st.getAddress1());
+//        }
 
-tx.commit();
+
+
+
+//
+//        // implementing native sql queries;
+//        List<Object[]> users = session.createNativeQuery("SELECT id1, name1, address1 FROM student_access").list();
+//
+//        for(Object[] row : users) {
+//            //System.out.println(Arrays.deepToString(row));
+//            System.out.println(row[2]);
+//        }
+
+
+
+
+        tx.commit();
         System.out.println("Registerd successfully");
          factory.close();
          session.close();
